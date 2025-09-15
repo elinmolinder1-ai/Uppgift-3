ffaf0331011045c2c242445934b98c35f71ec0b3
elinmolindr <elinmolinder1@gmail.com>
2025-09-15 12:48:35 +0200

    Changed text index and js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function clickBtn() {
-  document.getElementById("h1").innerHTML = 'Goodbye World';
-} 
+
+const title = document.querySelector("#title");
+const changeTextBtn = document.querySelector("#clickMe");
+
+
+changeTextBtn.addEventListener(
+  "click", addText
+);
+
+
+function addText(){
+ title.textContent = "GoodBye world!"
+}
